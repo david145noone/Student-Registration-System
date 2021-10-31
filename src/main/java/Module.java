77a4a51a34aa9ca652ba8e2a926104b063d4d5e7
David Noone <david145noone@gmail.com>
2021-10-31 22:40:00 +0000

    All tests done plus comments

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Module {
 
+    // fields for module class
     private String moduleName;
     private List<Student> students;
     private List<Program> programsAssociated;
@@ -15,6 +16,7 @@ public class Module {
     }
 
 
+    // accessor methods
     public String getModuleName() {
         return moduleName;
     }
@@ -27,12 +29,38 @@ public class Module {
         return programsAssociated;
     }
 
+    // add / remove methods
     public void addStudent(Student student) {
         students.add(student);
     }
 
+    public void removeStudent(Student student) {
+
+        if(students.contains(student)) {
+            students.remove(student);
+        }
+    }
+
     public void addProgram(Program program) {
         programsAssociated.add(program);
+    }
+
+    public void removeProgram(Program program) {
+
+        if(programsAssociated.contains(program)) {
+            programsAssociated.remove(program);
+        }
+    }
+
+    // A simpler method for displaying Module data in Driver program
+    @Override
+    public String toString() {
+        StringBuilder module = new StringBuilder("Module title: " + getModuleName() + "\n");
+        module.append("List of students enrolled: \n");
+        students.forEach(student -> module.append(student));
+        module.append("\nList of associated programs: \n");
+        programsAssociated.forEach(program -> module.append(program));
+        return module.toString();
     }
 }
 
--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,2 +1,57 @@
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ProgramTest {
+
+    Program program;
+
+    @Before
+    public void setUp() {
+        program = new Program("Computer Science",
+                DateTime.parse("01/01/1997",
+                        DateTimeFormat.forPattern("dd/MM/yyyy")),
+                DateTime.parse("01/01/1997",
+                        DateTimeFormat.forPattern("dd/MM/yyyy")));
+    }
+
+    @Test
+    public void testProgramName() {
+        Assert.assertEquals("Computer Science", program.getProgramName());
+    }
+
+    @Test
+    public void testAddStudent() {
+        Student mockStudent = new Student("John Smith",
+                20, DateTime.parse("01/01/1997", DateTimeFormat.forPattern("dd/MM/yyyy")));
+        program.addStudent(mockStudent);
+        Assert.assertTrue(program.getStudents().contains(mockStudent));
+    }
+
+    @Test
+    public void testRemoveStudent() {
+
+        Student mockStudent = new Student("John Smith",
+                20, DateTime.parse("01/01/1997", DateTimeFormat.forPattern("dd/MM/yyyy")));
+        program.addStudent(mockStudent);
+        program.removeStudent(mockStudent);
+        Assert.assertFalse(program.getStudents().contains(mockStudent));
+    }
+
+    @Test
+    public void testAddModule() {
+        Module mockModule = new Module("CT417");
+        program.addModule(mockModule);
+        Assert.assertTrue(program.getModules().contains(mockModule));
+    }
+
+    @Test
+    public void testRemoveModule() {
+        Module mockModule = new Module("CT417");
+        program.addModule(mockModule);
+        program.removeModule(mockModule);
+        Assert.assertFalse(program.getModules().contains(mockModule));
+    }
 }
